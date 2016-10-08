fdd1bde567e3e8503969dde3a7b8d3d277232651
Tai @ Flex <dch.tai@gmail.com>
2016-10-08 01:40:50 +0100

    add a customized message

--- init.lua
+++ init.lua
@@ -1,11 +1,13 @@
 local trollname = minetest.setting_get("rickroll.block") or "default:mese"
 local trollfreq = tonumber(minetest.setting_get("rickroll.chance")) or 1
 local soundname = minetest.setting_get("rickroll.sound") or "roll"
+local message = minetest.setting_get("rickroll.message") or "You have been rickrolled!"
 
 minetest.register_on_placenode(function(pos, newnode, placer, oldnode, itemstack, pointed_thing)
 	if math.random(1,trollfreq) ~= 1 then return end
 	local name = newnode.name
 	if name == trollname then
+		minetest.chat_send_player(placer:get_player_name(),"<rickroller>: "..message)
 		minetest.sound_play(soundname,{
 			pos = pos,
 			max_hear_distance = 10,
